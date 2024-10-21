22660c656abc448c11928742740e68f5537552f3
luizpalha <luizpalhaneto@hormail.com>
2024-10-21 20:35:05 -0300

    seconde Project

--- index.js
+++ index.js
@@ -1,36 +1,31 @@
-let nome = "Luizito";
-let experienciaXp = 9999;
-let categoria = "";
+let qtdVitorias = 106
+let qtdDerrotas = 2
+let categoria = ""
 
-console.log("Digite o nome do seu Heroi");
-console.log("________________");
-console.log("Neste momento o programa esta recebendo (get) o nome.");
-console.log("Digite o nivel de experiencia XP que ele se encontra");
-console.log("____________");
-console.log("Neste momento o programa esta recebendo (get) o nivel de experiencia.");
+let resultado = subtracao(qtdVitorias,qtdDerrotas)
 
-if (experienciaXp < 1000) {
-  categoria = "ferro";
-} else if (experienciaXp >= 1001 && experienciaXp <= 2000) {
-  categoria = "Bronze";
-} else if (experienciaXp >= 2001 && experienciaXp <= 5000) {
-  categoria = "Prata";
-} else if (experienciaXp >= 5001 && experienciaXp <= 7000) {
-  categoria = "Ouro";
-} else if (experienciaXp >= 7001 && experienciaXp <= 8000) {
-  categoria = "Platina";
-} else if (experienciaXp >= 8001 && experienciaXp <= 9000) {
-  categoria = "Ascendente";
-} else if (experienciaXp >= 9001 && experienciaXp <= 10000) {
-  categoria = "Imortal";
-} else if (experienciaXp > 10000) {
-  categoria = "***** Radiante *****";
-}
+if (resultado < 10) {
+    categoria = "ferro"
+  } else if (resultado >= 11 && resultado <= 20) {
+    categoria = "Bronze"
+  } else if (resultado >= 21 && resultado <= 50) {
+    categoria = "Prata"
+  } else if (resultado >= 51 && resultado <= 80) {
+    categoria = "Ouro"
+  } else if (resultado >= 81 && resultado <= 90) {
+    categoria = "Diamante"
+  } else if (resultado >= 91 && resultado <= 100) {
+    categoria = "Lendario"
+  } else if (resultado > 100) {
+    categoria = "Imortal"
+  }
 
-console.log("**** Processando ****");
-if (categoria === "***** Radiante *****") {
-  console.log("O heroi de nome " + nome + " esta no nivel: " + categoria);
-  console.log("VOCE ZEROU O JOGO!!!");
-} else {
-  console.log("O heroi de nome " + nome + " esta no nivel: " + categoria);
+console.log ("O Heroi tem saldo de: " + resultado + " e esta no nivel: " + categoria) 
+
+
+// Funcao subtracao: diminui quantidade de vitorias menos quantidade de derrotas
+function subtracao (numA, numB){
+let subtracao = numA - numB
+    return subtracao
+    
 }
